292707f1a4e8bd871391f9508909c81bd4b0e4d5
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-18 21:47:44 +0100

    working css

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -5,6 +5,7 @@ export default function Projects(): JSX.Element {
         <div className="project">
           <div className="project--header">
             <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
             <div className="project--link">
               <a
                 href="https://github.com/matbatten/portfolio-frontend"
@@ -14,170 +15,212 @@ export default function Projects(): JSX.Element {
                 <div>GitHub Repo</div>
               </a>
             </div>
-            <div className="project--link">You're already here!</div>
           </div>{" "}
           <br />
           <div className="project--desc">
             This was a project I brought together at the end of the taught phase
-            of my Academy training. I wanted to combine making a portfolio with
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">Yeezus inspiration</div>
+            <div className="project--link">
+              <a
+                href="https://yeebible.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>Deployed app</div>
+              </a>
+            </div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/Yeezus"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
-            producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            Having accepted Ye as my Lord and Saviour, I wanted a way to find
+            his inspiration in an easily-accessible fashion.<br/> Luckily for 
+            me, there is an existing API containing a near-limitless fountain
+            of wisdom.<br/> This app was built as a way to practice simple API
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
         </div>
 
         <div className="project">
-          <div className="project--name">React portfolio</div>
-          <a
-            href="https://github.com/matbatten/portfolio-frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="project--link">GitHub Repo</div>
-          </a>
-          <div className="project--link">You're already here!</div>
+          <div className="project--header">
+            <div className="project--name">React portfolio</div>
+            <div className="project--link">Link? You're already here!</div>
+            <div className="project--link">
+              <a
+                href="https://github.com/matbatten/portfolio-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>GitHub Repo</div>
+              </a>
+            </div>
+          </div>{" "}
+          <br />
           <div className="project--desc">
-            This was a project I brought toether at the end of the taugh phase
-            of my Academy training. I wanted to combine making a portfolio with
+            This was a project I brought together at the end of the taught phase
+            of my Academy training.<br/> I wanted to combine making a portfolio with
             producing a genuine full-stack app and work with some other concepts
-            that I felt I hadn't spent enough time on in the taught course. I
+            that I felt I hadn't spent enough time on in the taught course.<br/> I
             wanted to focus on having a tight front-end that utilised good
-            responsive styling. In futher development, I'm going to build a full
+            responsive styling.<br/> In futher development, I'm going to build a full
             back-end with a SQL database of all my projects, with an HTTP
             request to submit a new porject as a json.
           </div>
