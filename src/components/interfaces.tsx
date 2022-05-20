992d19ac3b9b2af6e749299e8ab9cc30fdbc5847
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-20 13:49:24 +0100

    added interface for a project

---
+++ src/components/interfaces.tsx
@@ -0,0 +1,10 @@
+
+export interface ProjectInterface {
+  projectid: number;
+  projectname: string;
+  projectlink: string;
+  projectrepo: string;
+  projectstack: string;
+  projectdescription: string;
+  projectimprovements: string;
+}
