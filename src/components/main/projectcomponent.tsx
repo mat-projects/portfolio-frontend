d813f64ab6a6318cc02947a9d72233a524225121
Mat Batten <matbat10@icloud.com>
2022-05-24 14:38:18 +0100

    formatting

--- src/components/main/projectcomponent.tsx
+++ src/components/main/projectcomponent.tsx
@@ -14,41 +14,41 @@ function ProjectComponent(): JSX.Element {
         return (
           <div key={id}>
             <div className="projectbox">
-            <div className="project">
-              <div className="project--header">
-                <div className="project--name">{project.project_name}</div>
-                <div className="project--links">
-                  <div className="project--link">
-                    <a
-                      href={project.project_link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Deployed App
-                    </a>
+              <div className="project">
+                <div className="project--header">
+                  <div className="project--name">{project.project_name}</div>
+                  <div className="project--links">
+                    <div className="project--link">
+                      <a
+                        href={project.project_link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Deployed App
+                      </a>
+                    </div>
+                    <div className="project--link">
+                      <a
+                        href={project.project_repo}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        GitHub Repo
+                      </a>
+                    </div>
                   </div>
-                  <div className="project--link">
-                    <a
-                      href={project.project_repo}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      GitHub Repo
-                    </a>
+                </div>
+                <div className="project--text">
+                  <div className="project--texttitle">Project Description</div>
+                  <div className="project--desc">
+                    {project.project_description}
+                  </div>
+                  <div className="project--texttitle">Project Improvements</div>
+                  <div className="project--impr">
+                    {project.project_improvements}
                   </div>
                 </div>
               </div>
-              <div className="project--text">
-                <div className="project--texttitle">Project Description</div>
-                <div className="project--desc">
-                  {project.project_description}
-                </div>
-                <div className="project--texttitle">Project Improvements</div>
-                <div className="project--impr">
-                  {project.project_improvements}
-                </div>
-              </div>
-            </div>
             </div>
           </div>
         );
